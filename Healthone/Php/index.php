fb9d3fc441724827cd2db27e6fcd8b786bcaca8e
Your Name <you@example.com>
2020-11-09 14:04:00 +0100

    begonnen aan html

---
+++ Healthone/Php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="all"/>
+    <meta name="language" content="Dutch">
+    <meta name="Author" content="Mark Van Den Burg">
+    <meta name="description" content="Healthone website">
+    <meta name="keywords" content="Doktoren, medicijnen apothekers">
+
+    <link rel="stylesheet" href="../Css/Main.css">
+    <title>Health One</title>
+</head>
+    <body>
+        <nav id="nav">
+            <a href="index.php"><img id="Home" src="Images/Logo.PNG" alt=""></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Registreren.php">Registreren</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
